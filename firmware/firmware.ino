b41cce2b88081a6565cec8626c3993f77f8da5a7
alticator <main@alticator.com>
2024-09-20 20:41:03 +0300

    Switch firmware sides
    Switch the firmwares between the satellite end and the receiver so that the one querying is the receiver and the responder is the satellite end.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,7 +1,6 @@
-#include <time.h>
 #include "CanSatNeXT.h"
 
-clock_t timeSave;
+int mode = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -9,66 +8,27 @@ void setup() {
   Serial.println("READY");
 }
 
-int checkInterrupt() {
-  if (Serial.available() != 0) {
-    String rstr = Serial.readString();
-    rstr.trim();
-    if (rstr == "Q") {
-      return 1;
-    }
-  }
-  return 0;
-}
-
 void loop() {
-  Serial.println("FUNC >> ");
-  while (Serial.available() == 0) {}
-  String reply = Serial.readString();
-  reply.trim();
-  if (reply == "MSG") {
-    sendData("BEGIN MSG");
-    int interrupt = 0;
-    while (!interrupt) {
-      Serial.println("MESSAGE (Q to quit) >> ");
-      while(Serial.available() == 0) {}
-      String msg = Serial.readString();
-      msg.trim();
-      if (msg.equalsIgnoreCase("Q") || msg.equalsIgnoreCase("QUIT")) {
-        sendData("END MODE");
-        interrupt = 1;
-      } else {
-        sendData(msg);
-        Serial.print(msg);
-        Serial.println(" [SENT]");
-      }
-    }
-  } else if (reply == "PING") {
-    sendData("PING");
-    timeSave = clock();
-  } else if (reply == "TMP") {
-    sendData("BEGIN TMP");
-    int interrupt = 0;
-    while (!interrupt) {
-      if (checkInterrupt()) {
-        sendData("END MODE");
-        interrupt = 1;
-      } else {
-        float temp = readTemperature();
-        Serial.print("Sending temperature data: ");
-        Serial.println(temp);
-        sendData(temp);
-        delay(200);
-      }
-    }
-  }
+
 }
 
 void onDataReceived(String data) {
   if (data == "PING") {
-    clock_t currentTime = clock();
-    float pingTime = (float)currentTime - (float)timeSave;
-    char timePrint[32];
-    sprintf(timePrint, "PING TIME: %.2f (FUNC) >>", pingTime);
-    Serial.println(timePrint);
+    sendData("PING");
+    Serial.println("RECEIVED AND RETURNED PING");
+  } else if (data == "BEGIN MSG") {
+    mode = 1;
+  } else if (data == "QUERY TMP") {
+    float temperature = readTemperature();
+    sendData("TMP " + (String)temperature);
+  } else if (data == "END MODE") {
+    mode = 0;
+  } else {
+    if (mode == 1) {
+      Serial.print("Received message: ");
+    } else {
+      Serial.print("Received unknown data: ");
+    }
+    Serial.println(data);
   }
 }
--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,6 +1,7 @@
+#include <time.h>
 #include "CanSatNeXT.h"
 
-int mode = 0;
+clock_t timeSave;
 
 void setup() {
   Serial.begin(115200);
@@ -8,28 +9,59 @@ void setup() {
   Serial.println("READY");
 }
 
-void loop() {
+int checkInterrupt() {
+  if (Serial.available() != 0) {
+    String rstr = Serial.readString();
+    rstr.trim();
+    if (rstr == "Q") {
+      return 1;
+    }
+  }
+  return 0;
+}
 
+void loop() {
+  Serial.println("FUNC >> ");
+  while (Serial.available() == 0) {}
+  String reply = Serial.readString();
+  reply.trim();
+  if (reply == "MSG") {
+    sendData("BEGIN MSG");
+    int interrupt = 0;
+    while (!interrupt) {
+      Serial.println("MESSAGE (Q to quit) >> ");
+      while(Serial.available() == 0) {}
+      String msg = Serial.readString();
+      msg.trim();
+      if (msg.equalsIgnoreCase("Q") || msg.equalsIgnoreCase("QUIT")) {
+        sendData("END MODE");
+        interrupt = 1;
+      } else {
+        sendData(msg);
+        Serial.print(msg);
+        Serial.println(" [SENT]");
+      }
+    }
+  } else if (reply == "PING") {
+    sendData("PING");
+    timeSave = clock();
+  } else if (reply == "TMP") {
+    sendData("QUERY TMP");
+    Serial.println("Sent data query for temperature");
+  } else {
+    Serial.println("ERROR : Unknown Function");
+  }
 }
 
 void onDataReceived(String data) {
   if (data == "PING") {
-    sendData("PING");
-    Serial.println("RECEIVED AND RETURNED PING");
-  } else if (data == "BEGIN MSG") {
-    mode = 1;
-  } else if (data == "BEGIN TMP") {
-    mode = 2;
-  } else if (data == "END MODE") {
-    mode = 0;
-  } else {
-    if (mode == 1) {
-      Serial.print("Received message: ");
-    } else if (mode == 2) {
-      Serial.print("Received temperature data: ");
-    } else {
-      Serial.print("Received unknown data: ");
-    }
-    Serial.println(data);
+    clock_t currentTime = clock();
+    float pingTime = (float)currentTime - (float)timeSave;
+    char timePrint[32];
+    sprintf(timePrint, "PING TIME: %.2f ms (FUNC) >>", pingTime);
+    Serial.println(timePrint);
+  } else if (data.startsWith("TMP")) {
+    data.replace("TMP ", "");
+    Serial.println("TEMPERATURE: " + data);
   }
 }
